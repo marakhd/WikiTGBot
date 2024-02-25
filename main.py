b41692fa07a7e187ad5f80279d1ccae5d7831195
marak <113286482+marakhd@users.noreply.github.com>
2024-02-25 12:14:53 +0300

    main

--- main.py
+++ main.py
@@ -20,22 +20,22 @@ dp = Dispatcher()
 #Хендлер для команды старт
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer(f'\tПривет! \n\n\tЭто бот Wikipedia.'
+    await message.answer(f'Привет! \n\nЭто бот Wikipedia.'
                          f'\nТы можешь отправить мне слово, а я найду его значение на Wikipedia.org')
 
 #Хендлер для команды why
 @dp.message(Command("why"))
 async def start(message: types.Message):
-    await message.answer(f'\tМы OpenSource(открытая разработка) проект телеграм бота википедии!'
+    await message.answer(f'Мы OpenSource проект телеграм бота википедии!'
                          f'\nВы можете наблюдать за разработкой на нашем <a href="{cfg.GITHUB}">GitHub</a>', disable_web_page_preview = True)
 
 #Хендлер для команды help
 @dp.message(Command("help"))
 async def start(message: types.Message):
-    await message.answer(f'\tЕсли бот не отвечает, значит он нашел несколько вариантов ответа. '
-                         f'\n\tПопробуйте задать вопрос конкретнее!'
-                         f'\n\tНапример:Python язык'
-                         f'\n\n\tПосле отправки запроса боту, нужно чуть чуть подождать(3-5сек)')
+    await message.answer(f'Если бот не отвечает, значит он нашел несколько вариантов ответа. '
+                         f'\nПопробуйте задать вопрос конкретнее!'
+                         f'\nНапример: <s>python</s> <b>Python язык</b>')
+    await message.answer(f'После отправки запроса боту, нужно чуть чуть подождать(3-5сек)')
 
 #Хендлер для текстовых запросов
 @dp.message(F.text)
@@ -49,7 +49,7 @@ async def search(message: types.Message):
 @dp.message()
 async def nottext(message: types.Message):
     if not message.text:
-        await message.answer(f'\tЭто не текст! \n\tДанный бот принимает только текстовые сообщение.')
+        await message.answer(f'Это не текст! \nДанный бот принимает только текстовые сообщение.')
 
 #Функция запуска пулинга
 async def main():
